fd8a63da469ab3f0fc54a6116ca728b26e2816eb
Akshit Agrawal <agrawalakshit001@gmail.com>
2023-04-14 22:23:35 +0530

    .

--- Contiguous Number Sum.py
+++ Contiguous Number Sum.py
@@ -1,3 +1,13 @@
-numbers = input("Enter Your Numbers With Comma: ")
-l1 = numbers.split(",")
-l1 = {int(x) for x in l1}
+numbers = input("Enter Your Numbers with commas: ")
+l1 = list(numbers.split(", "))
+l1 = list({int(x) for x in l1})
+max_sum = [0]
+x = 0
+while x < len(l1):
+    y = 1
+    checker = sum(l1[x:y])
+    if checker > sum(max_sum):
+        max_sum.clear
+        max_sum.append(checker)
+    y += 1
+print(max_sum)
