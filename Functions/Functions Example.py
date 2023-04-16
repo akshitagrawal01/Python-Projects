9d6368a13d5efd954906017df51bd0e9222f2def
Akshit Agrawal <agrawalakshit001@gmail.com>
2023-04-16 11:02:41 +0530

    Sorted

---
+++ Functions/Functions Example.py
@@ -0,0 +1,9 @@
+def greet_user(first_name):
+    print(f'Hi {first_name}')
+    print("Welcome Aboard")
+
+
+user_name = input("Enter Your Name: ")
+print("Start")
+greet_user(user_name)
+print("End")
